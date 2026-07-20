6925240b536d6de80201788bf4eec337e66012d1
FutureTech Admin <admin@futuretech.in>
2026-07-20 17:03:08 +0530

    Fix Vercel URL path routing with VercelPathFix WSGI middleware

--- api/index.py
+++ api/index.py
@@ -8,3 +8,21 @@ if root_dir not in sys.path:
 
 from app import app
 
+class VercelPathFix:
+    def __init__(self, wsgi_app):
+        self.wsgi_app = wsgi_app
+
+    def __call__(self, environ, start_response):
+        matched_path = environ.get('HTTP_X_MATCHED_PATH')
+        if matched_path:
+            environ['PATH_INFO'] = matched_path
+        else:
+            path_info = environ.get('PATH_INFO', '')
+            if path_info.startswith('/api/index'):
+                new_path = path_info[10:]
+                environ['PATH_INFO'] = new_path if new_path != '' else '/'
+        return self.wsgi_app(environ, start_response)
+
+app.wsgi_app = VercelPathFix(app.wsgi_app)
+
+
